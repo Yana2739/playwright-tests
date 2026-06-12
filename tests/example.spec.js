c5d8a1b993e8268154a0d7eb28dfe4850d1f6794
Яна Мащенко <yanamashchenko@MacBook-Air-Dmitro-2.local>
2026-06-12 15:15:56 +0300

    Add registration form tests

--- tests/example.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test')
-
-test('Open qauto homepage', async ({ page }) => {
-
-  await page.goto('/')
-
-  await expect(page).toHaveTitle(/Hillel Qauto/)
-
-})
